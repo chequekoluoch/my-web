4ef1875630413acc25b50719f55ac380626ab531
makombekepha <makombekepha@gmail.com>
2020-09-25 22:31:02 +0300

    react website first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return ( 
+  <Router>
+    <NavBar />
+    <Switch>
+      <Route path='/' exact component={Home}/>
+      <Route path='/products' component={Products}/>
+      <Route path='/services' component={Services}/>
+      <Route path='/sign-up'  component={SignUp}/>
+    </Switch>
+  </Router>
+  );
+}
+
+export default App;
